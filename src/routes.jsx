f14c92ef3ee9cee043e37583f1cdb7f689f38150
Jonathan Silva <jonathanedsilva@gmail.com>
2024-06-16 17:47:27 -0300

    add third challenge

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
+import App from './App.tsx'
 import MinimalBlogCard from './challenges/minimal-blog-card/minimal-blog-card.tsx'
 import BusinessBlogCard from './challenges/business-blog-card/business-blog-card.tsx'
-import App from './App.tsx'
+import TestimonialPage from './challenges/testimonial-page/testimonial-page.jsx'
 
 const routes = [
   {
@@ -14,6 +15,10 @@ const routes = [
   {
     path: '/business-blog-card',
     element: <BusinessBlogCard />,
+  },
+  {
+    path: '/testimonial-page',
+    element: <TestimonialPage />,
   },
 ]
 
